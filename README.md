458780bf5bd52af9839b6e1436c897649e7f67b5
Eileen D'Angelo <eileenrkerekes@gmail.com>
2022-07-25 15:24:36 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,46 @@
+# Frontend Mentor - Expenses chart component solution
+
+This is a solution to the [Expenses chart component challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/expenses-chart-component-e7yJBUdjwt). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+  - [Built with](#built-with)
+- [Author](#author)
+
+
+### The challenge
+
+Users should be able to:
+
+- View the bar chart and hover over the individual bars to see the correct amounts for each day
+- See the current day's bar highlighted in a different colour to the other bars
+- View the optimal layout for the content depending on their device's screen size
+- See hover states for all interactive elements on the page
+- **Bonus**: See dynamically generated bars based on the data provided in the local JSON file
+
+### Screenshot
+
+![screen shot of full screen webpage](https://github.com/Eileenpk/Expenses-chart-component/blob/main/images/Screenshot-full-website.png)
+![screen shot of mobile webpage](https://github.com/Eileenpk/Expenses-chart-component/blob/main/images/Screenshot-mobile.png)
+
+### Links
+
+- Solution URL: [GitHub](https://github.com/Eileenpk/Expenses-chart-component)
+- Live Site URL: [live site]( https://eileenpk.github.io/Expenses-chart-component/)
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- Mobile-first workflow
+- Vanilla JavaScript
+- Fetch
+
+## Author
+
+- Frontend Mentor - [@Eileenpk](https://www.frontendmentor.io/profile/Eileenpk)
---
+++ index.js
@@ -0,0 +1,67 @@
+
+// get the data from data.json
+fetch('./data.json')
+    .then(res => res.json())
+    .then(data => {
+        graphData(data)
+    })
+
+// inserts the data into the graph, styles it, and inserts tool tip 
+const graphData = data => {
+    const graphContainer = document.querySelector('.week-graph-container')
+
+    let graphContainerHtml = ''
+    // loops over data
+    for (let i = 0; i < 7; i++) {
+        
+        graphContainerHtml += `      
+        <section class="weekday-container">
+            <span class="hovertext" data-hover="$${data[i].amount}">   
+                <div class="${data[i].day} ${currentDay(data[i].day)} day bar" style="height:calc(${data[i].amount}px * 2.5 );"</div>
+            </span>
+            <p class="day">${data[i].day}</p>
+      </section>`
+      
+    }
+    graphContainer.innerHTML = graphContainerHtml
+    // currentDay(data)
+}
+
+// target the day of the week it is currently and change the color of the bar
+
+const currentDay = data => {
+    const curDate = new Date
+    let currday = curDate.getDay()
+    
+    switch(currday) {
+        case 0:
+            currday = 'sun' 
+            break;
+        case 1:
+            currday = 'mon' 
+            break;
+        case 2:
+            currday = 'tue'
+            break;
+        case 3:
+            currday = 'wed' 
+            break;
+        case 4: currday = 'thu' 
+            break;
+        case 5:
+            currday = 'fri' 
+            break;
+        case 6:
+            currday = 'sat' 
+            break;
+        default:
+    }
+
+        if(currday == data) {
+            return 'current-day'
+        }
+        else {
+            return
+        }
+    }
+
